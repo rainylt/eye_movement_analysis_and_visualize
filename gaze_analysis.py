1d96981bd6852ef1b19c7e76f53603a5b11f4a88
rainylt <35332416+lt1103725556@users.noreply.github.com>
2021-12-02 15:10:54 +0800

    add draw timeline function for one user

--- gaze_analysis.py
+++ gaze_analysis.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 import numpy as np
+import pandas as pd
 import sys, os
 import event_detection as ed
 from config import names as gs
 from config import conf
 import json
+import matplotlib.pyplot as plt
 import pdb
 
 
@@ -70,15 +72,41 @@ class gazeAnalysis (object):
 		mean_duration = np.mean(duration)/30 #frames/30 = time
 		gaze_freq = len(self.fixations)
 		vel = 0
-		rad = 0
+		max_a = 0
+		mean_a = 0
 		for sac in self.saccades:
 			vel += sac[9]
-			rad += sac[4]
+			max_a += sac[11]
+			mean_a += sac[12]
 		mean_vel = vel/len(self.saccades)
-		mean_rad = rad/len(self.saccades)
-		stat = [mean_duration, gaze_freq, mean_vel, mean_rad, len(self.saccades)]
+		mean_max_angle = max_a/len(self.saccades)
+		mean_angle = mean_a/len(self.saccades)
+		stat = [mean_duration, gaze_freq, mean_vel, mean_max_angle, mean_angle, len(self.saccades)]
 
 		return stat
+
+	def time_series_analysis(self):
+		'''
+		draw the curve of the indicators
+		扫视的峰值速度、最大角度、平均角度的变化[9,11,12]
+		:return:
+		'''
+		x_timeline = [x for x in range(len(self.saccades))]
+		y_vel = [x[9] for x in self.saccades]
+		y_max_angle = [x[11] for x in self.saccades]
+		y_mean_angle = [x[12] for x in self.saccades]
+		plt.plot(x_timeline, y_vel, 'o-',label='vel')
+		plt.plot(x_timeline, y_max_angle,'o-', label = 'max_angle')
+		plt.plot(x_timeline, y_mean_angle, 'o-',label = 'mean_angle')
+		plt.title('saccade event indicator analysis')
+		plt.xlabel('event index')
+		#plt.ylabel('')
+		plt.savefig('output/saccade_analysis.png')
+
+
+
+
+
 
 def json2np(eye_data):
 	event_list = []
@@ -96,6 +124,51 @@ def json2np(eye_data):
 		frame_idx += 1
 	gaze_points = np.array(event_list)
 	return gaze_points
+
+def search_data(path, filename):
+	'''
+	search file in path
+	:param path: root path storing files
+	:param filename: the name to be searched
+	:return: searched file path list
+	'''
+	result = []
+	for root, dirs, files in os.walk(path):
+		for file in files:
+			if(file==filename):
+				file_path = os.path.join(root, file)
+				result.append(file_path)
+	return result
+
+def get_dir_stat(path, filename):
+	'''
+	get statistic information from all the eye.json in database
+	:param path: database path
+	:param filename: eye.json
+	:return: all features, [num_person, stat_dim]
+	'''
+	file_list = search_data(path, filename)
+	stat_array = np.array([])
+	for file_path in file_list:
+		with open(file, 'r') as f:
+			file = json.load(f)
+		eye_data = file["gazePoints"]
+		gaze_points = json2np(eye_data)
+		extractor = gazeAnalysis(gaze_points, conf.fixation_radius_threshold, conf.fixation_duration_threshold,
+								 conf.saccade_min_velocity, conf.max_saccade_duration)
+		feature = extractor.stat_analysis()
+		stat_array.extend(feature)
+	return stat_array
+
+def analyze_avg_feature(stat_array):
+	'''
+	indicator:
+	num_person
+
+	'''
+	df = pd.DataFrame(stat_array, columns=['gaze_duration','num_gaze','sac_peak_vel','sac_max_angle','mean_angle','num_sac'])
+	df.describe()
+
 if __name__ == '__main__':
 	with open('data/JsonData/eye_576.json', 'r') as f:
 		file = json.load(f)
